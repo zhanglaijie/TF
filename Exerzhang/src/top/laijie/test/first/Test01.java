fe5c17af1b09cef55930b50ad61933ec9bac8450
zhanglaijie <laijie100@qq.com>
2015-11-14 12:00:22 +0800

    ttttest

---
+++ Exerzhang/src/top/laijie/test/first/Test01.java
@@ -0,0 +1,10 @@
+package top.laijie.test.first;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git-hub");
+	}
+
+}
